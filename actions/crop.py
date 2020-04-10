36dccfcd8132acd63f8f669660167702aba64980
Shmulik Regev <shmul@catonetworks.com>
2020-04-10 16:08:53 +0300

    export cropped file reliably

--- actions/crop.py
+++ actions/crop.py
@@ -4,6 +4,7 @@ import typing
 import pytimeparse
 import pathlib
 import csv
+import os
 
 Msecs = typing.NewType('Msecs', int)
 AudioFiles = typing.NewType('AudioFiles', typing.List[str])
@@ -21,6 +22,7 @@ def offset(hms: str, sec: float) -> Msecs:
 # start/end are of the format [hh:]mm:ss; head/tail are secs
 def crop(file: str, start: str, end: str, head: float, tail: float,
          play: bool):
+    file = os.path.realpath(file)
     cropped = pydub.AudioSegment.from_file(pathlib.Path(file))
     ln = len(cropped)
     if tail == 0:
@@ -31,8 +33,18 @@ def crop(file: str, start: str, end: str, head: float, tail: float,
     segment = cropped[s:e]
     if play:
         pydub.playback.play(segment)
+        return
+
+    cropped = os.path.join(os.path.dirname(file), "cropped")
+    os.makedirs(cropped, exist_ok=True)
+    target = os.path.join(cropped, os.path.basename(file))
+    if os.path.exists(target):
+        os.remove(target)
+    if s == 0 and e == ln:
+        print("softlink")
+        os.symlink(file, target)
     else:
-        pass  # todo - save the file
+        segment.export(target)
 
 
 def crop_many(csvfile: str) -> AudioFiles:
@@ -40,7 +52,7 @@ def crop_many(csvfile: str) -> AudioFiles:
     with open(csvfile, newline='') as file:
         for row in csv.reader(file):
             audio = crop(row['file'], row['start'], row['end'], row['head'],
-                         row['tail'])
+                         row['tail'], False)
             audios.append(audio)
 
     return crop(file, start, end, head, tail)
--- actions/play.py
+++ actions/play.py
@@ -1,11 +1,13 @@
 import pydub
 import pydub.playback
+import os
 import pathlib
 
 
 def playall(head: float, tail: float, files: str):
     for f in files:
-        audio = pydub.AudioSegment.from_file(pathlib.Path(f))
+        f = os.path.realpath(f)
+        audio = pydub.AudioSegment.from_file(f)
         ln = len(audio)
         if head == 0 and tail == 0:
             pydub.playback.play(audio)
